9b14e63e122b573186baf33b90be7b759a0e7530
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-06 02:15:15 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -12,21 +12,21 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   dns_prefix          = "${var.terrafform_aks_name}"
   kubernetes_version  = "${var.terraform_aks_kubernetes_version}"
 
-  linux_profile {
-    admin_username = "${var.terraform_azure_admin_name}"
-
-    ssh_key {
-      key_data = "${file("${var.terraform_azure_ssh_key}")}"
-    }
-  }
-
   agent_pool_profile {
     name    = "agentpool"
     count   = "${var.terraform_aks_agent_vm_count}"
     vm_size = "${var.terraform_aks_vm_size}"
-    vnet_subnet_id  = "${azurerm_subnet.my-subnet.id}"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
   }
-
+  
+    linux_profile {
+    admin_username = "${var.terraform_azure_admin_name}"
+    ssh_key {
+      key_data = "${file("${var.terraform_azure_ssh_key}")}"
+    }
+  }
+   
   service_principal {
     client_id     = "${var.terraform_azure_service_principal_client_id}"
     client_secret = "${var.terraform_azure_service_principal_client_secret}"
