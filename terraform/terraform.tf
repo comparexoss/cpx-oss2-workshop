c20297a0c068aa39d2722f4a9727c2a4522441a6
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-05 23:44:26 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "test" {
 
 # create azure container service (aks)
 resource "azurerm_kubernetes_cluster" "test" {
-  name                = "devopshkkaks"
+  name                = "${var.terrafform_aks_name}"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   dns_prefix          = "${azurerm_resource_group.test.name}"
