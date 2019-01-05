c4084a658dd1df87ec27dcc6ca793e55ef1ce1d0
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-06 02:03:34 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   name                = "${var.terrafform_aks_name}"
   location            = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
-  dns_prefix          = "${azurerm_resource_group.k8s.name}"
+  dns_prefix          = "${var.terrafform_aks_name}"
   kubernetes_version  = "${var.terraform_aks_kubernetes_version}"
 
   linux_profile {
@@ -24,6 +24,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     name    = "agentpool"
     count   = "${var.terraform_aks_agent_vm_count}"
     vm_size = "${var.terraform_aks_vm_size}"
+    vnet_subnet_id  = "${azurerm_subnet.my-subnet.id}"
   }
 
   service_principal {
