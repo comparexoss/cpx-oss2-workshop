4aa0b2688d5c71bd4888d8b9794be2903b86a56f
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-06 00:48:24 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,22 +1,15 @@
-provider "azurerm" {
-    subscription_id = "db29d5ad-1fbb-4443-b3c5-94c79b4250dc"
-    client_id       = "07e3e541-57d3-4591-b7eb-705da6838259"
-    client_secret   = "5d1c2325-5493-425f-aee5-e37fb2ffffa6"
-    tenant_id       = "bd200d3d-aa96-41ae-8c56-0bd57c973985"
-}
-
 # create a resource group if it doesn't exist
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "k8s" {
   name     = "${var.terraform_azure_resource_group}"
   location = "${var.terraform_azure_region}"
 }
 
 # create azure container service (aks)
-resource "azurerm_kubernetes_cluster" "test" {
+resource "azurerm_kubernetes_cluster" "k8s" {
   name                = "${var.terrafform_aks_name}"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  dns_prefix          = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.k8s.location}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
+  dns_prefix          = "${azurerm_resource_group.k8s.name}"
   kubernetes_version  = "${var.terraform_aks_kubernetes_version}"
 
   linux_profile {
