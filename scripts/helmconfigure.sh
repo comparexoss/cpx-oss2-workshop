4b3714a804b399502940d93b2c7bd169890b92c9
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-06 13:22:04 +0300

    Update helmconfigure.sh

--- scripts/helmconfigure.sh
+++ scripts/helmconfigure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-MYPATH="/usr/local/bin/"
-sudo /usr/local/bin/kubectl create serviceaccount --namespace kube-system tiller
-sudo /usr/local/bin/kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-sudo /usr/local/bin/kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-sudo /usr/local/bin/helm init --service-account tiller
+AKSNAME=devopshkkaks
+RGNAME=mstrdevopsaksrg
+sudo az aks install-cli
+sudo az aks get-credentials --resource-group $RGNAME --name $AKSNAME
+sudo az aks browse --resource-group $RGNAME --name $AKSNAME
+sudo kubectl create serviceaccount --namespace kube-system tiller
+sudo kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+sudo kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+sudo helm init --service-account tiller
