ee54673714ed5fd7b9204cecc687c0b86e06acf2
Hakki Ogretmen <45731819+comparexoss@users.noreply.github.com>
2019-01-08 20:45:46 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
     resource "null_resource" "create_folder" {
       provisioner "local-exec" {
-        command = "mkdir -p ~/.kube/config"
+        command = "mkdir -p ~/.kube"
       }
       depends_on = ["azurerm_kubernetes_cluster.k8s"]
     }
